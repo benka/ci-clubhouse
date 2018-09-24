42c1f19db8d5cc44e07f744f5b8265e92c96ab77
benka <andrew.benkoczi@gmail.com>
2018-09-24 14:27:31 -0400

    feat: update with project_id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import Clubhouse from 'clubhouse-lib';
+var Clubhouse = require('clubhouse-lib');
 
 const WORKFLOW_COMPLETED = 500000002;
 const WORKFLOW_IN_DEV = 500000004;
 const WORKFLOW_READY_REVIEW = 500000005;
 const WORKFLOW_READY_DEPLOY = 500000006;
-const project_id = [ 108, 725 ];
+const project_id = { "web": 108, "ps": 725 };
 const clientConfig = {
   baseURL: 'https://api.clubhouse.io',
   version: 'v2'
@@ -12,27 +12,37 @@ const clientConfig = {
 const updateParams = { 'workflow_state_id': WORKFLOW_COMPLETED };
 
 try {
+  var id = project_id.web;
+  if (process.argv.length > 2) {
+    process.argv.forEach(function(arg) {
+      console.log("arg:", arg);
+      if (arg.startsWith("--project_id")) {
+        id = arg.split('=')[1]
+      }
+    })
+    console.log(id);
+  } 
+
   if (!process.env.CLUBHOUSE_API_TOKEN) {
     throw 'ERROR: CLUBHOUSE_API_TOKEN environment variable missing.';
   }
+
   const api_token = process.env.CLUBHOUSE_API_TOKEN;
   const client = Clubhouse.create(api_token, clientConfig);
 
-  project_id.forEach(function(id) {
+  client.listStories(id).then(
+    cards => {
+      // filter for READY_TO_DEPLOY status
+      console.log("Project [",id,"] Cards found: ", cards.length);
 
-    client.listStories(id).then(
-      cards => {
-        // filter for READY_TO_DEPLOY status
-        console.log("Project [",id,"] Cards found: ", cards.length);
+      const cardsReady = cards.filter(card => card.workflow_state_id == WORKFLOW_READY_DEPLOY);
+      cardsReady.forEach(function(card) {
+        console.log(" > [",id,"] ", card.id);
+        client.updateStory(card.id, updateParams);
+      });
+    }
+  );
 
-        const cardsReady = cards.filter(card => card.workflow_state_id == WORKFLOW_READY_DEPLOY);
-        cardsReady.forEach(function(card) {
-          console.log(" > [",id,"] ", card.id);
-          client.updateStory(card.id, updateParams);
-        });
-      }
-    );
-  });
 } catch (e) {
   console.log(`\n${e}\n\n`)
 }
